6da32457d11fa76afe930070fea0cc7027df08e0
Jeong-DongHee <weg9511@gmail.com>
2021-10-12 00:12:50 +0900

    출금 수정
    출금액보다 잔액이 작을 경우 '잔액이 부족합니다'라는 메시지 출력

--- Project1/WithdrawMoney.cpp
+++ Project1/WithdrawMoney.cpp
@@ -26,9 +26,12 @@ void Withdraw() {
 
 	for (int i = 0; i < Account::GetAccNum(); i++) {
 		if (account[i]->GetAccID() == accID) {
-			account[i]->Withdraw(money);
-			cout << "출금완료" << endl;
-			return;
+			if (account[i]->GetBalance() >= money) {
+				account[i]->Withdraw(money);
+				cout << "출금완료" << endl;
+				return;
+			}
+			cout << "잔액이 부족합니다." << endl;
 		}
 	}
 	cout << "없는 계좌입니다." << endl;
